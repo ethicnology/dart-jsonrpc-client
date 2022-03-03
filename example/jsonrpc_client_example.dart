48b4ee34be0dbae56a35b08c03b0ebd7b6d13d86
ethicnology <ethicnology@pm.me>
2022-03-03 11:17:19 +0100

    simple-package template

---
+++ example/jsonrpc_client_example.dart
@@ -0,0 +1,6 @@
+import 'package:jsonrpc_client/jsonrpc_client.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/jsonrpc_client.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library jsonrpc_client;
+
+export 'src/jsonrpc_client_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
